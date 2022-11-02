74712eec682dcbe0d2d0acd9d827e241ee500e7b
kalvin <kalvinyu0@hotmail.com>
2022-11-02 01:01:01 +0000

    got my js working with the html yeeha!

--- inputDisplay.js
+++ inputDisplay.js
@@ -1,88 +1,67 @@
+let eq
+let x = null
+let bracketNum = 0
+let k = 0
+let pre = 0
+
+class subEqCreator {
+    constructor() {
+	this.precede = 0;
+	this.string = '';
+    }
+    parser() {
+	this.string += eq[k]
+    }
+};
 function trigger() {
-    let userInput;
     show();
-    parser();
+    brackets();
+    subEqDynamic()
+    parseEquation();
 };
 function show() {
     const recent = document.querySelector("ol");
     const txt = document.getElementById('input');  
-    userInput = txt.value;
+    eq = txt.value;
     li = document.createElement('li');
-    li.innerText =  userInput;
+    li.innerText = eq;
     recent.appendChild(li);    
     txt.value = '';
 };
-function parser() {
-    const display = document.getElementById("display");
-    let equation;
-    let letters = 'abcdefghijklmnopqrstuvwxyz';
-    letters = letters.split('');
-    let opening = [];
-    let holder = [''];
-   if (/=(?=\w)/.test(userInput)) {}
-    else {
-    userInput += "=0";
-    }
-    display.value = userInput;
-    equation = userInput.split('')
-
+function brackets() {
+    for (let i = 0; i < eq.length; i++) {
+	if (eq[i] == "(") {
+	    bracketNum++;
+	};
+    };
+};
+let subEqDynamic = function() {
+    for (let count = 0; count <= bracketNum; count++) {
+        this['subEq' + count] = x =  new subEqCreator() 
+    };
+};
+function parseEquation() {
+    let nextIndex = 0; 
+    let currentIndex = 0; 
+    let lastIndex = [0]; 
+    let eqLen = eq.length;
+    let nex = this['subEq' + nextIndex]
     
-    /*  const bracketsMarker = () => {
-        equation.forEach((element, index) => {
-        if (element == '(') {
-        opening.unshift(index);
+    for (k; k < eqLen; k++) {           
+        if (eq[k] == "(") {          
+            nextIndex++;
+	    pre++
+            this['subEq' + currentIndex].string += '()';
+	    this['subEq' + nextIndex].precede = pre;
+            lastIndex.push(currentIndex);
+            currentIndex = nextIndex;
         }
-        });
-        opening.push(0);
-        letters.length = opening.length;
-        console.log(equationArr);
-        console.log(opening);
-	};*/
-    
-    function parserBrackets() {
-	let nextVar = 0;
-	let currentVar = 0;
-	let prev = [0]
-	equation.forEach((element, index) => {
-	    let i = equation[index];
-
-	    if (i == '(') {
-		if (holder.length -1 < currentVar) {
-		    holder.push('')
-		}
-		nextVar++;
-		holder[currentVar] += '[' + nextVar + ']';
-		prev.push(currentVar);
-		currentVar = nextVar;
-	    }
-	    
-	    else if ( i == ')') {
-		currentVar = prev.pop();
-		
-	    }
-	    else {
-		if (holder.length -1 < currentVar) {
-		    holder.push('');
-		}
-		holder[currentVar] += i;
-	    }
-	});
-	console.log(holder.length)
-	return holder
-    }
-    
-    let vars = function () {
-        for(let count = 0; count <= holder.length; count++) {
-            eval('let LHS ' + count + ' = ' + holder[count] + ';' );
-	  
-        };
-    }
-    // bracketsMarker();
-    parserBrackets();
-    vars();q
-    //equation = userInput.split('=')    //var LHS = equation.slice(0, 1),
-      //  RHS = equation.slice(1);
-    //equation = [LHS, RHS];
-    //equation.join(=)
-
- };
+        else if (eq[k] == ")") {
+	    pre--
+	    currentIndex = lastIndex.pop();
+        }
+        else {          
+	    this['subEq' + currentIndex].parser()
+        }       
+    }    
+}
